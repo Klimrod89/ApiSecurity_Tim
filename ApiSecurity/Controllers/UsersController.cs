14e10fc23880a7f3893d1d2f6d5262d26d701528
Yassir C <51804657+Klimrod89@users.noreply.github.com>
2024-03-29 00:58:02 -0400

    Did not forget this time to commit. Learned about minimal api

--- ApiSecurity/Controllers/UsersController.cs
+++ ApiSecurity/Controllers/UsersController.cs
@@ -7,5 +7,22 @@ namespace ApiSecurity.Controllers;
 [ApiController]
 public class UsersController : ControllerBase
 {
-    
+    private readonly IConfiguration config;
+
+    public UsersController(IConfiguration config)
+    {
+        this.config = config;
+    }
+
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] {"value1", "value2"};
+    }
+
+    [HttpGet("id")]
+    public string Get(string id)
+    {
+        return config.GetConnectionString(id)! /*?? throw new Exception("Missing connection string")*/;
+    }
 }
